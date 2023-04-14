6ae59c44db1111aac4e6756cd5c2bd6cc12498ba
Ivan Blancarte <iblancarte583@gmail.com>
2023-04-14 10:10:21 -0600

    Viernes14Abril2023

--- CuidadorReneweed/build/web/js/CuidadorScript.js
+++ CuidadorReneweed/build/web/js/CuidadorScript.js
@@ -1,5 +1,6 @@
 
 let datos;
+let cuidador;
 const modal_container = document.getElementById('modal_container');
 
 function ver(){
@@ -16,4 +17,32 @@ function logout(){
 
 function cargarMapa() {
     window.location.replace('../mapa/mapa.html');
+}
+
+function cargarDatos(){
+    cuidador=JSON.parse(localStorage.getItem("currentUser"));
+    let nombre=cuidador.persona.nombre;
+    let primerApell=cuidador.persona.primerApellido;
+    let segundoApell=cuidador.persona.segundoApellido;
+    let rfc=cuidador.rfc;
+    let celCasa=cuidador.numTelefonoCasa;
+    let telMovil=cuidador.numTelefonoMovil;
+    let genero=cuidador.persona.genero;
+    let nomUser=cuidador.usuario.nombreUsuario;
+    let contrasenia=cuidador.usuario.contrasenia;
+    let emai=cuidador.correo;
+    
+    
+    document.getElementById("txtNombreCuidador").value=nombre;
+    document.getElementById("txtPrimerApellidoCuidador").value=primerApell;
+    document.getElementById("txtSegundoApellidoCuidador").value=segundoApell;
+    document.getElementById("txtRFC").value=rfc;
+    document.getElementById("txtTelefonoCasaCuidador").value=celCasa;
+    document.getElementById("txtTelefonoMovilCuidador").value=telMovil;
+    document.getElementById("txtGeneroCuidador").value=genero;
+    document.getElementById("txtUsuarioCuidador").value=nomUser;
+    document.getElementById("txtPasswordCuidador").value=contrasenia;
+    document.getElementById("txtEmail").value=emai;
+    
+    
 }
